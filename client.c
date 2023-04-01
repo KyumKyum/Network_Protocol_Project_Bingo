2b543c9c3a9d1980310382af5b1bf01033b2c86c
KyumKyum <mycheesepasta@gmail.com>
2023-04-01 23:21:03 +0900

    Check Bingo

--- client.c
+++ client.c
@@ -7,6 +7,8 @@
 #include <sys/socket.h>
 
 #define BUF_SIZE 512 //* Definition for Buffer size;
+#define REQ_SIZE 10
+#define NUM_SIZE 5
 #define NUM_LIMIT 25 //* Definition for number limit in bingo game.
 
 void error_handling(char *);
@@ -14,9 +16,16 @@ void bingo(int);
 void bingo_init(void);    //* Intialize bingo
 void bingo_shuffle(void); //* Shuffle the bingo
 void bingo_print(void);   //* Print bingo plates
+void bingo_select(int);
+int bingo_calculate(void);
+void bingo_calculate_row(int *);
+void bingo_calculate_col(int *);
+void bingo_calculate_diag(int *);
+void bingo_send_msg(void);
 int handle_server_msg(char *, char *);
 
 int bingo_value[25] = {0}; //* bingo_value: show bingo number, became -1 if the number checked.
+int bingo_num = 0;         //* bingo_num: show number of bingo current player get.
 
 int main(int argc, char *argv[])
 {
@@ -50,7 +59,7 @@ int main(int argc, char *argv[])
   }
   else
   {
-    printf("Loading...\n");
+    printf("상대방을 기다리고 있습니다...\n");
     bingo(sock); //* Start Bingo Game - Client
   }
 
@@ -61,12 +70,20 @@ int main(int argc, char *argv[])
 //* Function bingo: Start and manage whole process of bingo game.
 void bingo(int sock)
 {
-  int str_len, flag; //* flag: variable to determine to start, continue, or terminate the bingo game.
+  int str_len, flag, bingo_num = 0; //* flag: variable to determine to start, continue, or terminate the bingo game.
   char message[BUF_SIZE];
+  char request[REQ_SIZE];
+  char number[NUM_SIZE];
   char *delimiter = " ";
+
+  bingo_init();
 
   while (1)
   {
+    //* Reset value.
+    flag = 0;
+    bingo_num = 0;
+
     //* Read Message from the server
     str_len = read(sock, message, strlen(message));
     /*
@@ -78,27 +95,36 @@ void bingo(int sock)
 
     if (str_len > 0) //* Works if sth arrives
     {
+      printf("%s\n", message);
       char *state = strtok(state, delimiter);
       char *value = strtok(NULL, delimiter);
 
-      // flag = handle_server_msg(state, value); //* Handle Server-side message
+      flag = handle_server_msg(state, value); //* Handle Server-side message
       //: Based on the flag value, the client will determine to start, continue or terminate the bingo game.
       /*
       > Messages
-      * 0: PENDING - Waiting for another client join to this game.
+      * 0: init value, used for pending
       * 1: START_FIRST - Start game, current client first.
       * 2: START_SECOND - Start game, opponent first.
       * 3: SELECTED <number> - Opponent selected number, end its turn.
       * 4: FINISHED WIN | LOSE - Game Finished, show result, and show if current user win or lose based on the value.
       * -1: REJECTED | <Other Error> - Connection Rejected or Error Happened, close the connection.
       */
-      // TODO - Need to parse flag.
+    }
+    // TODO - Need to parse flag.
+    if (flag == 0)
+    { //! value 0 is only for debug uses - need to change 1 after finishing
+      //* Case 1 Start First
+      bingo_print();
+      fputs("숫자를 입력해주세요: ", stdout);
+      fgets(number, BUF_SIZE, stdin);
+
+      bingo_select(atoi(number));
+      bingo_num = bingo_calculate();
+
+      printf("%d %d\n", bingo_num, atoi(number));
     }
   }
-
-  bingo_init();
-  bingo_print();
-
   return;
 }
 
@@ -134,7 +160,7 @@ void bingo_print(void)
   {
     if (bingo_value[idx] == -1)
     { //* If this value had been checked, prints blank.
-      printf("    ");
+      printf("  X ");
     }
     else
     {
@@ -148,16 +174,117 @@ void bingo_print(void)
   }
 }
 
+void bingo_select(int selected)
+{
+  for (int idx = 0; idx < 25; idx++)
+  {
+    if (bingo_value[idx] == -1 || bingo_value[idx] != selected)
+    {
+      //* If current value is already selected, or not the selected
+      continue;
+    }
+    //* Founded.
+    bingo_value[idx] = -1;
+    break;
+  }
+}
+
+int bingo_calculate(void)
+{
+  int num_of_bingo = 0;
+
+  bingo_calculate_row(&num_of_bingo);
+  bingo_calculate_col(&num_of_bingo);
+  bingo_calculate_diag(&num_of_bingo);
+
+  return num_of_bingo;
+}
+
+void bingo_calculate_row(int *num_of_bingo) //* Calculate number of bingo in row.
+{
+  //*calculate the row
+  for (int row = 0; row < 5; row++)
+  {
+    for (int element = 0; element < 5; element++)
+    {
+      if (bingo_value[(row * 5) + element] != -1)
+      { //*Current row is not a bingo
+        break;
+      }
+      if (element == 4)
+      { //* Reached end, current row is a bingo.
+        *num_of_bingo += 1;
+      }
+    }
+  }
+}
+
+void bingo_calculate_col(int *num_of_bingo) //* Calculate number of bingo in column.
+{
+  //*calculate the column
+  for (int col = 0; col < 5; col++)
+  {
+    for (int element = 0; element < 5; element++)
+    {
+      if (bingo_value[col + (element * 5)] != -1)
+      { //*Current column is not a bingo
+        break;
+      }
+      if (element == 4)
+      { //* Reached end, current column is a bingo.
+        *num_of_bingo += 1;
+      }
+    }
+  }
+}
+
+void bingo_calculate_diag(int *num_of_bingo) //* Calculate number of bingo in diagonal direction.
+{
+  //* calculate right diagonal
+  for (int rdiag = 0; rdiag < 5; rdiag++)
+  {
+    if (bingo_value[rdiag * 6] != -1)
+    {
+      //* Current Diagonal Line is not a bingo
+      break;
+    }
+
+    if (rdiag == 4)
+    {
+      *num_of_bingo += 1;
+    }
+  }
+
+  //* calculate left diagonal
+  for (int ldiag = 0; ldiag < 5; ldiag++)
+  {
+    if (bingo_value[(ldiag + 1) * 4] != -1)
+    {
+      //* Current Diagonal Line is not a bingo.
+      break;
+    }
+
+    if (ldiag == 4)
+    {
+      *num_of_bingo += 1;
+    }
+  }
+}
+
+void bingo_send_msg(void)
+{
+}
+
 int handle_server_msg(char *state, char *value)
 {
   int ret_val = -1;
 
-  if (strcmp(state, "PENDING") == 0)
-  {
-    printf("상대방을 기다리고 있습니다...");
-    ret_val = 0;
-  }
-  else if (strcmp(state, "START_FIRST") == 0)
+  // if (strcmp(state, "PENDING") == 0)
+  // {
+  //   printf("상대방을 기다리고 있습니다...");
+  //   ret_val = 1;
+  // }
+  if (strcmp(state, "START_FIRST") == 0)
   {
     printf("상대방을 찾았습니다! 게임이 시작됩니다.\n 당신은 선공입니다.\n");
     ret_val = 1;
